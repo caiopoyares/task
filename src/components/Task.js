bea5775230f31e8a4e0ca32e3622967e4ab28c1a
Caio Macbook <capoyares@gmail.com>
2019-05-10 14:39:32 -0300

    fixed task line

--- src/components/Task.js
+++ src/components/Task.js
@@ -3,14 +3,20 @@ import React from "react";
 const Task = props => {
   return (
     <li>
-      <input className="checkbox" type="checkbox" id={props.id} />
-      <label htmlFor={props.id}>{props.children}</label>
-      <span
-        className="delete_btn"
-        onClick={props.handleDelete.bind(this, props.id)}
-      >
-        &times;
-      </span>
+      <div>
+        <input className="checkbox" type="checkbox" id={props.id} />
+        <label htmlFor={props.id}>
+          <span className="task__text">{props.children}</span>
+        </label>
+      </div>
+      <div className="delete_box">
+        <span
+          className="delete_btn"
+          onClick={props.handleDelete.bind(this, props.id)}
+        >
+          &times;
+        </span>
+      </div>
     </li>
   );
 };
