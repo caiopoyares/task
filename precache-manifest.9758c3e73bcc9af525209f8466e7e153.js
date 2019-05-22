14d17a3bbf04632435ed78d04fa82acac743099f
Caio Macbook <capoyares@gmail.com>
2019-05-22 14:54:09 -0300

    Updates

---
+++ precache-manifest.9758c3e73bcc9af525209f8466e7e153.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8b38072400d097e5d30958de4cdd08",
+    "url": "/projects/task/index.html"
+  },
+  {
+    "revision": "5d333ce6ea3c80a9dee3",
+    "url": "/projects/task/static/css/main.03bb4ef6.chunk.css"
+  },
+  {
+    "revision": "9e5b0139dd4874814c8a",
+    "url": "/projects/task/static/js/2.72539f5f.chunk.js"
+  },
+  {
+    "revision": "5d333ce6ea3c80a9dee3",
+    "url": "/projects/task/static/js/main.fc21ea97.chunk.js"
+  },
+  {
+    "revision": "fce911c543e1fa5158e1",
+    "url": "/projects/task/static/js/runtime~main.97915c05.js"
+  }
+]);
